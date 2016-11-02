5d665a32d319e8a020ad8d44be35b503a2fb0e9b
spark <spark@MacBook-Air-2.local>
2016-11-02 09:39:05 -0700

    Add controller action and corresponding view for Toys show under Pet ID

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -3,4 +3,9 @@ class ToysController < ApplicationController
     @pet = Pet.find(params[:pet_id])
     @toys = @pet.toys
   end
+
+  def show
+    @pet = Pet.find(params[:pet_id])
+    @toy = Toy.find(params[:id])
+  end
 end
